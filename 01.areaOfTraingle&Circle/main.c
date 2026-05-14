bf0d54e0868c4150c98c1115d4637634de76a1b7
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-15 02:34:49 +0600

    area of triangle & circle

---
+++ 01.areaOfTraingle&Circle/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    ///------ area of triangle -------
+
+   //int b,h;
+   //printf("enter base:");
+   //scanf("%d",&b);
+    //printf("enter height:");
+   //scanf("%d",&h);
+   //float area = 0.5*b*h;
+   //printf("%.2f",area);
+
+   ///------ area of circle -------
+
+   int r;
+   scanf("%d",&r);
+   float area = 3.1415*r*r;
+   printf("%f",area);
+
+   return 0;
+}

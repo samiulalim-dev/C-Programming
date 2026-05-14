dd93e898a05fda1a59ae3cf3c085a76b12161818
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-15 04:06:03 +0600

    added 1 to n series using for and while loop

---
+++ 02.ifelseProgram/1_to_n_series/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    ///----- while loop -----
+
+    int n;
+    scanf("%d",&n);
+    //int i = 1;
+    //while(i <= n){
+    //    printf("%d \n",i);
+    //    i++;
+    //}
+    ///------ for loop ------
+
+    for(int i=1; i <=n;i++){
+        printf("%d \n",i);
+    }
+
+    return 0;
+}

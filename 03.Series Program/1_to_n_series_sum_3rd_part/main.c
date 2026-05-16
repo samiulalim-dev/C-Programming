65aa0b33b51d552f68af982c6aefc9d1f8140d65
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-17 00:39:55 +0600

    series program complete

--- 03.Series Program/1_to_n_series_sum_3rd_part/main.c
+++ 03.Series Program/1_to_n_series_sum_3rd_part/main.c
@@ -27,15 +27,48 @@ int main()
      //}
     //printf("%.4f",sum);
 
-      ///======= 1/1^2+1/2^2+1/3^2+1/4^2+ ------- +nth =======
+    ///======= 1/1^2+1/2^2+1/3^2+1/4^2+ ------- +nth =======
+
+    //int n;
+    //scanf("%d",&n);
+    //float sum = 0;
+    // for(int i=1;i<=n;i++){
+     //       sum= sum +1.0/(i*i);
+     //}
+    //printf("%.4f",sum);
+
+    ///======= 1/1-1/2+1/3-1/4+ ------- +nth =======
+
+    //int n;
+    //scanf("%d",&n);
+    //float sum = 0;
+    //for(int i=1;i<=n;i++){
+     //   if(i%2 ==0){
+       //     sum = sum - (1.0/i);
+       // }
+       // else{
+         //   sum = sum + (1.0/i);
+       // }
+    // }
+   // printf("%.4f",sum);
+
+   ///======== 1/1+11/3+21/5+31/7+ -------- +nth ========
 
     int n;
     scanf("%d",&n);
     float sum = 0;
-     for(int i=1;i<=n;i++){
-            sum= sum +1.0/(i*i);
-     }
-    printf("%.4f",sum);
+    int s1=1;
+
+    for(int i=1;i<=n;i=i+2){
+        sum = sum + (1.0*s1/i);
+        s1= s1+10;
+    }
+    printf("%.3f",sum);
+
+
+
+
+
 
     return 0;
 }

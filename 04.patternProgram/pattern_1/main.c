609d3e27c3419bbfb5d110f142987861ac2ce426
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-18 01:00:21 +0600

    added pattern programm

--- 04.patternProgram/pattern_1/main.c
+++ 04.patternProgram/pattern_1/main.c
@@ -3,17 +3,77 @@
 int main()
 {
     ///========= pattern 1 =========
+    /**
+     3
+     3 9
+     3 9 27
+    */
 
+    //int n,row,col;
+    //scanf("%d",&n);
+    //for( row=1;row<=n;row++){
+       // int s1=3,s2=3;
+       // for(col=1;col<=row;col++){
+       //     printf(" %d",s1);
+       //     s1=s1*s2;
+      //  }
+   // printf("\n");
+   // }
+
+    ///======== pattern 2 ========
+    /**
+    * a
+    * b c
+    * d e f
+    */
+//    int n,row,col;
+//    scanf("%d",&n);
+//    char ch='a';
+//    for(row=1;row<=n;row++){
+//        for(col=1;col<=row;col++){
+//            printf("%c ",ch);
+//            ch++;
+//        }
+//    printf("\n");
+//    }
+
+ ///========= pattern 3 =========
+ /***
+ 1
+ 1 2
+ 1 2 6
+ 1 2 6 24
+ */
+//   int n,row,col;
+//   scanf("%d",&n);
+//    for( row=1;row<=n;row++){
+//    int s1=1,s2=2;
+//    for(col=1;col<=row;col++){
+//        printf(" %d",s1);
+//        s1=s1*s2;
+//        s2=s2+1;
+//      }
+//   printf("\n");
+//
+// }
+
+ ///========= pattern 3 =========
+ /***
+ 1
+ 2 1
+ 3 2 1
+ 4 3 2 1
+ */
     int n,row,col;
     scanf("%d",&n);
     for( row=1;row<=n;row++){
-        int s1=3,s2=3;
-        for(col=1;col<=row;col++){
-            printf(" %d",s1);
-            s1=s1*s2;
+        for(col=row;col>=1;col--){
+            printf(" %d",col);
         }
     printf("\n");
     }
+
+
 
     return 0;
 }

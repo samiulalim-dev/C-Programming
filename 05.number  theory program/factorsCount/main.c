21dd7b1160d1db63707d791722b930b44d052025
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-18 19:46:24 +0600

    complete factors number

---
+++ 05.number  theory program/factorsCount/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    ///========= factors count ========
+    int n;
+    scanf("%d",&n);
+    int res = 0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            res++;
+        }
+    }
+   printf("%d",res);
+
+
+   ///========= factors print ========
+//    int n;
+//    scanf("%d",&n);
+//    for(int i=1;i<=n;i++){
+//        if(n%i==0){
+//            printf("%d",i);
+//        }
+//    }
+
+     ///========= factors sum ========
+//    int n;
+//    scanf("%d",&n);
+//    int sum = 0;
+//    for(int i=1;i<=n;i++){
+//        if(n%i==0){
+//           sum = sum + i;
+//        }
+//    }
+//    printf("%d",sum);
+
+
+    return 0;
+}

acd948f130f6e7b18be572d5bb424231b905d3ee
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-19 21:41:35 +0600

    added unit convertion

---
+++ 4NumberAvg/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    float res = (a+b+c+d)/4;
+    printf("%.4f",res);
+
+    return 0;
+}
---
+++ unitConvertion/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    ///======== Temperature =======
+
+//    float F;
+//    scanf("%f",&F);
+//    float con = ((F-32)/9)*5;
+//    printf("%.4f",con);
+//    return 0;
+
+    ///======== meter to feet ========
+
+    int m;
+    scanf("%d",&m);
+    float feet = m*3.28084;
+    printf("%f",feet);
+
+}

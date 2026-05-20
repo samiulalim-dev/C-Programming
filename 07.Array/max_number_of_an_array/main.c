4fc511d3b46542a6f158a6466f1472ca46d6fd2f
samiulalim-dev <samiulalim.dev@gmail.com>
2026-05-21 02:20:14 +0600

    sum and avg of an array

--- 07.Array/max_number_of_an_array/main.c
+++ 07.Array/max_number_of_an_array/main.c
@@ -3,6 +3,40 @@
 int main()
 {
     ///======== Max number of an array ========
+//   int n;
+//   printf("enter array size:");
+//   scanf("%d",&n);
+//   int a[n];
+//   for(int i=0;i<n;i++){
+//    printf("Enter your %d number:",i+1);
+//    scanf("%d",&a[i]);
+//   }
+//   int max=a[0];
+//   for(int i=0;i<n;i++){
+//    if(a[i]>max){
+//        max = a[i];
+//    }
+//   }
+//   printf("%d ",max);
+
+   ///======== sum of an array ==========
+//
+//   int n;
+//   printf("enter array size:");
+//   scanf("%d",&n);
+//   int a[n];
+//   for(int i=0;i<n;i++){
+//    printf("Enter your %d number:",i+1);
+//    scanf("%d",&a[i]);
+//   }
+//   int sum=0;
+//   for(int i=0;i<n;i++){
+//   sum = sum+a[i];
+//   }
+//   printf("%d ",sum);
+
+    ///======== AVG of an array ==========
+
    int n;
    printf("enter array size:");
    scanf("%d",&n);
@@ -11,12 +45,12 @@ int main()
     printf("Enter your %d number:",i+1);
     scanf("%d",&a[i]);
    }
-   int max=a[0];
+   float sum=0;
    for(int i=0;i<n;i++){
-    if(a[i]>max){
-        max = a[i];
-    }
+   sum = sum+a[i];
    }
-   printf("%d ",max);
+   printf("%.2f ",sum/n);
+
+
     return 0;
 }

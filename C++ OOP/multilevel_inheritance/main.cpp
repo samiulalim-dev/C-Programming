4915ab07aae4d205431d111b2c1e0277ebd4e15d
samiulalim-dev <samiulalim.dev@gmail.com>
2026-06-15 00:00:17 +0600

    added constructor and destructor

--- C++ OOP/multilevel_inheritance/main.cpp
+++ C++ OOP/multilevel_inheritance/main.cpp
@@ -1,30 +1,124 @@
 #include <iostream>
 using namespace std;
-//!========== multileve inheritance ============
-class animal{
+//!========== multilevel inheritance ============
+//class animal{
+//public:
+//    void eat(){
+//    cout << "eating"<<endl;
+//    }
+//};
+//class dog:public animal{
+//    public:
+//    void bark(){
+//    cout<< "bark"<<endl;
+//    }
+//};
+//
+//class puppy: public dog{
+//public :
+//    void weep(){
+//        cout <<"weep"<<endl;
+//    }
+//};
+//int main()
+//{
+//    puppy p;
+//    p.eat();
+//    p.bark();
+//    p.weep();
+//    return 0;
+//}
+
+
+//!============ multiple inheritance ==========
+
+//class father{
+//    public:
+//    void working(){
+//    cout<<"Father is working"<<endl;
+//    }
+//};
+//class mother{
+//    public:
+//    void cooking(){
+//    cout<<"mother is cooking"<<endl;
+//    }
+//};
+//class child:public father,public mother{
+//public:
+//    void study(){
+//    cout<<"child is studying"<<endl;
+//    }
+//};
+//
+//int main(){
+//child c;
+//c.working();
+//c.cooking();
+//c.study();
+//
+//return 0;
+
+//}
+
+//!========== hybrid inheritance ============
+//class animal{
+//public:
+//    void eat(){
+//    cout << "eating"<<endl;
+//    }
+//};
+//class dog:public animal{
+//    public:
+//    void bark(){
+//    cout<< "bark"<<endl;
+//   }
+//};
+//class cat : public animal{
+//public:
+//    void meow(){
+//        cout<<"meow"<<endl;
+//    }
+//
+//};
+//
+//class puppy: public dog , public cat{
+//public :
+//   void weep(){
+//        cout <<"weep"<<endl;
+//   }
+//};
+//int main()
+//{
+//    puppy p;
+//    p.bark();
+//    p.meow();
+//    p.weep();
+//    return 0;
+//}
+
+//!=========== destructor ===========
+class mark{
 public:
-    void eat(){
-    cout << "eating"<<endl;
+    mark(){
+        cout<< "inside constructor"<<endl;
     }
-};
-class dog:public animal{
-    public:
-    void bark(){
-    cout<< "bark"<<endl;
+    ~mark(){
+        cout << "inside destructor"<<endl;
     }
 };
 
-class puppy: public dog{
-public :
-    void weep(){
-        cout <<"meow"<<endl;
-    }
-};
-int main()
-{
-    puppy p;
-    p.eat();
-    p.bark();
-    p.weep();
+int main(){
+    mark ml;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
